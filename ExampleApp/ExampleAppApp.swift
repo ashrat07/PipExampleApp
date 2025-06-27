87366c13b8b8f8c4dc2045676627846d0dfc8df4
Ashish Rathore <asrathore@microsoft.com>
2025-06-27 11:11:00 +0530

    Initial Commit

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Ashish on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleAppTests/ExampleAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleAppTests.swift
+//  ExampleAppTests
+//
+//  Created by Ashish on 27/06/25.
+//
+
+import Testing
+
+struct ExampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
